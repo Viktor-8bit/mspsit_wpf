74cf368f075e3e8fb8b25cee386075ae9f95c3ef
Кожевников Иван <64777603+Viktor-8bit@users.noreply.github.com>
2024-11-12 23:21:21 +0700

    init commit

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace mispisit_wpf;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace mispisit_wpf;
+
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+    
+    private void myButton_Click(object sender, RoutedEventArgs e)
+    {
+        TextBox textBox = (TextBox)this.FindName("number")!;
+        int numb = System.Convert.ToInt16(textBox.Text);
+        MessageBox.Show($"Ваше число в 10 степени {Math.Pow(numb, 10)}");
+    }
+
+    private void text_updater(object sender, RoutedEventArgs e)
+    {
+        TextBox textBox = (TextBox)this.FindName("number")!;
+        Label myLabel = (Label)this.FindName("myLabel")!;
+        myLabel.Content = $"Вы ввели {textBox.Text}";
+    }
+}
